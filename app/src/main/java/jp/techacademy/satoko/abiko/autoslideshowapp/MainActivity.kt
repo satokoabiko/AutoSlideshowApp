6d4766bcc6b10d4f6a784c79b0e2063adddfe7f3
satoko <mocamoca777@gmail.com>
2023-05-25 22:33:46 +0900

    修正１

--- app/src/main/java/jp/techacademy/satoko/abiko/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoko/abiko/autoslideshowapp/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.techacademy.satoko.abiko.AutoSlideshowApp
 
 import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -28,13 +29,22 @@ class MainActivity : AppCompatActivity() {
     private val readImagesPermission =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) android.Manifest.permission.READ_MEDIA_IMAGES
         else android.Manifest.permission.READ_EXTERNAL_STORAGE
-
-//再生・進むなどのボタンが表示されない
+    private var cursor: Cursor? = null
+    //再生・進むなどのボタンが表示されない
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+           null, // 項目（null = 全項目）
+           null, // フィルタ条件（null = フィルタなし）
+           null, // フィルタ用パラメータ
+           null // ソート (nullソートなし）
+        )
 
         binding.button1.setOnClickListener {
             // パーミッションの許可状態を確認する
@@ -46,44 +56,55 @@ class MainActivity : AppCompatActivity() {
                 Log.d("ANDROID", "許可されていない")
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissionLauncher.launch(readImagesPermission)
-            //拒否の場合の処理
-                //？？
             }
         }
-    }
-    // 画像をボタンで表示する
- //   private var cursor: Cursor? = null
-    private fun getContentsInfo() {
-        // 画像の情報を取得する(cursol の初期化)
-        val resolver = contentResolver
-  //Q：もとはvalだが、cursorは変数扱いとみなしたほうがよいか？ボタンによって内容が変わる？
-        var cursor = resolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-            null, // 項目（null = 全項目）
-            null, // フィルタ条件（null = フィルタなし）
-            null, // フィルタ用パラメータ
-            null // ソート (nullソートなし）
-        )
-
-       // 進むボタンクリック時の処理（次の画像がない場合、movetonext）
-       // cursol を使って moveToNext() し、画像を表示する
-   //Q： 代入の書き方、カーソルとmovetoの記述
+        //再生ボタン（タイマーで動作する）
         binding.startBotton.setOnClickListener {
-            cursor!!.moveToNext()
+//            Log.d("ANDROID", "start")   timer
+//            cursor!!.moveToNext()
         }
+        //進むボタン　
+        binding.nextButton.setOnClickListener {
+            if (cursor!!.moveToNext()) {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-        binding.resetButton.setOnClickListener {
-            // 戻るボタンクリック時の処理
-            // cursol を使って moveToPrevious() し、画像を表示する
-            // val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            cursor!!.moveToPrevious()
+                binding.imageView.setImageURI(imageUri)
+            }
+            //最後の場合、最初を表示する
+            else if (cursor!!.moveToFirst()) {
+                     val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                     val id = cursor!!.getLong(fieldIndex)
+                     val imageUri =
+                         ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                     binding.imageView.setImageURI(imageUri)
+            }
         }
+        //戻るボタン
+        binding.previousButton.setOnClickListener {
+        if (cursor!!.moveToPrevious()) {
+           // indexからIDを取得し、そのIDから画像のURIを取得する
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-        binding.resetButton.setOnClickListener {
-            // 再生停止ボタンクリック時の処理
-            // cursol を使って movetoFirst() し、画像の再表示する（onCreate？）
+            binding.imageView.setImageURI(imageUri)
+
+            }
+          //最初の場合、最後を表示
+          else  if (cursor!!.moveToFirst()) {
+              val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+              val id = cursor!!.getLong(fieldIndex)
+              val imageUri =
+                  ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+              binding.imageView.setImageURI(imageUri)
+          }
         }
     }
-//カーソルクローズ：カーソルフェッチと対？
-   //     cursor.close()
 }
